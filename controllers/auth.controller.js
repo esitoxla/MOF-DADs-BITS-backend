0a35e05a1ca09d530118e0f228c6aa50483b0d08
Irene Toxla <toxlairene@gmail.com>
2025-10-24 20:47:33 +0000

    updated login endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -84,7 +84,7 @@ export const login = async (req, res, next) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       secure: true,
-      sameSite: none,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 
