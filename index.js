305e6b87902cb2fa0a917de6639140f0f775a5ed
Irene Toxla <toxlairene@gmail.com>
2025-10-25 08:20:26 +0000

    updated the index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://mof-dads-bits.netlify.app"],
     credentials: true,
   })
 );
