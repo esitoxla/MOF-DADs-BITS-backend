03d817613c454ccedc91873c427a3fb3d2f53bc9
Irene Toxla <toxlairene@gmail.com>
2026-03-04 12:36:07 +0000

    trigger a redeploy

--- index.js
+++ index.js
@@ -35,10 +35,6 @@ app.use(
 
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log("Incoming request:", req.method, req.originalUrl);
-  next();
-});
 
 //routes
 app.use("/api/auth", authRoute);
