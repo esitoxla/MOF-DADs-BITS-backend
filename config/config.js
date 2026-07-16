b81f613793ab3d592b537d0ca0ea2f8afa0f4aac
Irene Toxla <toxlairene@gmail.com>
2026-07-16 19:44:17 +0000

    Configure backend for Aive

--- config/config.js
+++ config/config.js
@@ -7,19 +7,22 @@ export default {
     password: process.env.DB_PASS || null,
     database: process.env.DB_NAME || "mof_db",
     host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 3306,
+    port: Number(process.env.DB_PORT) || 3306,
     dialect: "mysql",
   },
+
   production: {
-    username: process.env.DB_PROD_USER,
-    password: process.env.DB_PROD_PASS,
-    database: process.env.DB_PROD_NAME,
-    host: process.env.DB_PROD_HOST,
-    port: process.env.DB_PROD_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     dialect: "mysql",
-  },
-  production: {
-    use_env_variable: "DATABASE_URL", 
-    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
